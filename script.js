b5c5245ab6b8eb21db8a042c79722be13e2f0400
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2023-05-31 22:25:12 +0300

    fixes

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const popup = document.getElementById('popup')
-const popupButton = document.getElementById('popup-button')
-const popupButtonClose = document.getElementById('popup-close')
+const popupButtons = document.querySelectorAll('[data-popup-button]')
 
 const loadButton = document.getElementById('load-items')
 const hiddenItems = document.querySelectorAll('[data-item-hidden]')
 
-const copied = document.getElementById('copied')
-const copiedButton = document.getElementById('copied-button')
 
 hiddenItems.forEach((item) => {
     item.classList.add('hidden')
@@ -19,17 +15,100 @@ loadButton.addEventListener('click', () => {
     loadButton.classList.add('hidden')
 })
 
-popupButton.addEventListener('click', (e) => {
-    if(!e.target.closest('#popup')) {
-        popup.classList.toggle('open')
+
+popupButtons.forEach((item, index) => {
+
+    let popup = `<div class="popup" data-popup=${index + 1} >
+                    <div class="popup__wrapper" data-copied-button>
+                        <div data-copied class="popup__copied">
+                            <div class="popup__copied-icon"></div>
+                            <div class="popup__copied-text">Code copied to clipboard.</div>
+                        </div>
+                        <div class="popup__close" data-popup-close></div>
+                       
+
+                        <div class="popup__title">20 Free Spins at 21 Dukes Casino</div>
+
+                        <div class="popup__promo">WELCOME</div>
+
+                        <div class="popup__columns popup__columns--1">
+
+                            <div class="popup__column popup__column--1">
+                                Games allowed:
+                            </div>
+                            <div class="popup__column popup__column--2">
+                                Slots, Keno, Scratch, Cards, Bingo
+                            </div>
+                            
+                        </div>
+
+                        <div class="popup__columns popup__columns--2">
+
+                            <div class="popup__column popup__column--1">
+                                Max cash out:
+                            </div>
+                            <div class="popup__column popup__column--2">
+                                $100
+                            </div>
+
+                        </div>
+
+                        <div class="popup__columns">
+
+                            <div class="popup__column popup__column--1">
+                                Min deposit:
+                            </div>
+                            <div class="popup__column popup__column--2 green">
+                                FREE
+                            </div>
+
+                        </div>
+                    </div>
+                </div>`
+
+    item.insertAdjacentHTML('beforeend', popup)
+})
+
+
+let popups = document.querySelectorAll('[data-popup]')
+
+popups.forEach((item, index) => {
+
+    popupButtons.forEach((button, index2) => {
+        button.addEventListener('click', (e) => {
+            if (index === index2 && !e.target.closest('[data-popup-close]')) {
+                if (!e.target.closest('.popup')) {
+                    item.classList.toggle('open')
+                }
+
+            } else {
+                item.classList.remove('open')
+                copied.classList.remove('show')
+            }
+
+        })
+    })
+
+    const popupButtonClose = item.querySelector('[data-popup-close]')
+    const copiedButton = item.querySelector('[data-copied-button]')
+    const copied = item.querySelector('[data-copied]')
+
+
+    popupButtonClose.addEventListener('click', () => {
+        item.classList.remove('open')
         copied.classList.remove('show')
+    })
+
+
+    copiedButton.addEventListener('click', () => {
+        copied.classList.add('show')
+    })
+})
+
+document.addEventListener('click', (e) => {
+    if(!e.target.closest('.popup') && !e.target.closest('.list__item-info')) {
+        popups.forEach((item) => {
+            item.classList.remove('open')
+        })
     }
-})
-
-popupButtonClose.addEventListener('click', () => {
-    popup.classList.remove('open')
-})
-
-copiedButton.addEventListener('click', () => {
-    copied.classList.add('show')
 })
